a27723a180738af5b8a868c0f6b641ab46eb2564
Atishay Jain <61509695+atishay2305-hub@users.noreply.github.com>
2020-05-13 04:17:13 +0530

    Add files via upload

---
+++ PHOTOBLOG/photoblog.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("feedback").innerHTML = "Thank You";
+  }
+  function myFunction() {
+    document.getElementById("feedback1").innerHTML = "Will definitely improve, thank you.";
+  }
